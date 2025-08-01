9cd7780470cd8657801f5af3ccf1bc2d59705226
Sean B. Palmer <sean@miscoranda.com>
2025-08-01 17:23:54 +0100

    Add a release policy page

--- atr/routes/root.py
+++ atr/routes/root.py
@@ -17,7 +17,10 @@
 
 """root.py"""
 
+from typing import Final
+
 import asfquart.session
+import htpy
 import quart.wrappers.response as response
 import sqlalchemy.orm as orm
 import sqlmodel
@@ -28,6 +31,37 @@ import atr.routes as routes
 import atr.template as template
 import atr.user as user
 import atr.util as util
+
+_POLICIES: Final = htpy.div[
+    htpy.h1["Release policy"],
+    htpy.p[
+        """Note that the ATR platform will replace the use
+        dist.apache.org svn repository where mentioned in
+        any of the following policies."""
+    ],
+    htpy.h2["Standard ASF policies"],
+    htpy.ul[
+        htpy.li[htpy.a(href="https://www.apache.org/legal/release-policy.html")["Release policy"],],
+        htpy.li[htpy.a(href="https://www.apache.org/legal/src-headers.html")["Source headers"],],
+        htpy.li[htpy.a(href="https://www.apache.org/legal/resolved.html")["Third party license"],],
+        htpy.li[htpy.a(href="https://www.apache.org/foundation/voting.html")["Voting process"],],
+        htpy.li[htpy.a(href="https://infra.apache.org/release-publishing.html")["Release process"],],
+    ],
+    htpy.h2["Additional incubator policies"],
+    htpy.ul[
+        htpy.li[
+            htpy.a(href="https://incubator.apache.org/policy/incubation.html#releases")["Incubator release process"],
+        ],
+        htpy.li[
+            htpy.a(href="https://incubator.apache.org/guides/releasemanagement.html#podling_constraints")[
+                "Incubator constraints"
+            ],
+        ],
+        htpy.li[
+            htpy.a(href="https://incubator.apache.org/policy/incubation.html#disclaimers")["Incubation disclaimer"],
+        ],
+    ],
+]
 
 
 @routes.public("/")
@@ -103,6 +137,11 @@ async def index() -> response.Response | str:
 
     # Public view
     return await template.render("index-public.html")
+
+
+@routes.public("/policies")
+async def policies() -> str:
+    return await template.blank("Policies", content=_POLICIES)
 
 
 @routes.committer("/todo", methods=["POST"])
