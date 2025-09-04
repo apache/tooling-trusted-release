4a527dd9ee07981e2602f946063cbcdcda872f75
Sean B. Palmer <sean@miscoranda.com>
2025-09-04 19:43:55 +0100

    Add server defaults to the workflow fields migration

--- migrations/versions/0026_2025.09.04_eb02c4d9.py
+++ migrations/versions/0026_2025.09.04_eb02c4d9.py
@@ -19,9 +19,9 @@ depends_on: str | Sequence[str] | None = None
 
 def upgrade() -> None:
     with op.batch_alter_table("releasepolicy", schema=None) as batch_op:
-        batch_op.add_column(sa.Column("github_compose_workflow_path", sa.String(), nullable=False))
-        batch_op.add_column(sa.Column("github_vote_workflow_path", sa.String(), nullable=False))
-        batch_op.add_column(sa.Column("github_finish_workflow_path", sa.String(), nullable=False))
+        batch_op.add_column(sa.Column("github_compose_workflow_path", sa.String(), nullable=False, server_default=""))
+        batch_op.add_column(sa.Column("github_vote_workflow_path", sa.String(), nullable=False, server_default=""))
+        batch_op.add_column(sa.Column("github_finish_workflow_path", sa.String(), nullable=False, server_default=""))
         batch_op.drop_column("github_workflow_path")
 
 
